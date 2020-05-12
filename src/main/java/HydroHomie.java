babba8215268cbcf8f1757ca2ecf267d59cd23a0
Joe <jhd35@bath.ac.uk>
2020-05-12 15:16:57 +0100

    Initial commit

---
+++ src/main/java/HydroHomie.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HydroHomie extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+        primaryStage.setTitle("HydroHomie REMASTERED EDITION");
+        primaryStage.setScene(new Scene(root, 580, 398));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+        primaryStage.setMinWidth(primaryStage.getWidth());
+        primaryStage.setMinHeight(primaryStage.getHeight());
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
